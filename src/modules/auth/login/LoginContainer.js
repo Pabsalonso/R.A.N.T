8478b36ec5d8c3d52865e0cc93fec21d8fc811d2
Pabsalonso <56518151+Pabsalonso@users.noreply.github.com>
2022-04-30 13:55:56 +0100

    Componente login cambiado a class component Spinner añadido a login

--- src/modules/auth/login/LoginContainer.js
+++ src/modules/auth/login/LoginContainer.js
@@ -1,60 +1,81 @@
 import { View, Text, Button } from 'native-base';
 import React from 'react';
+import BaseComponent from 'base/BaseComponent';
+
 import { TextInput } from 'react-native-gesture-handler';
 import * as FormValidator from '../../../utils/validators/FormValidators';
 // import * as Routing from 'routes/Routing';
-import { Alert, ActivityIndicator } from 'react-native';
-// import Spinner from 'react-native-loading-spinner-overlay';
+import { Alert } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 import { login } from 'services/api/ApiCalls';
+import { setUserTokens } from '../../../services/user/UserActions';
+import * as UserReducer from '../../../services/user/UserReducer';
 
-export default function LoginContainer() {
+class LoginContainer extends BaseComponent {
 //   const [spinner, setSpinner] = React.useState(false);
-  const [email, onChangeEmail] = React.useState('');
-  const [password, onChangePassword] = React.useState('');
+//   const [email, onChangeEmail] = React.useState('');
+//   const [password, onChangePassword] = React.useState('');
+  constructor(props) {
+    super(props);
+    this.state = {
+      spinner: false,
+      email: '',
+      password: '',
+      prueba: '' };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-  const onSubmit = () => {
-    // setSpinner(true);
-    if (!FormValidator.emailValidator(email)
-     || !FormValidator.isValidPassword(password)) {
-      Alert.alert('Error', 'Email o contraseña no cumple con requisitos');
-    } else {
-      login(email, password).then((result) => {
-        // if (!result.ok) {
-        //   Alert.alert('Error', 'Este correo no vale');
-        // }
-        console.log(result.token);
-      });
-    }
-    // setSpinner(false);
-  };
+   onSubmit = () => {
+     if (!FormValidator.emailValidator(this.state.email)
+       || !FormValidator.isValidPassword(this.state.password)) {
+       Alert.alert('Error', 'Email o contraseña no cumple con requisitos');
+     } else {
+       this.setState({ spinner: true });
+       login(this.state.email, this.state.password).then((result) => {
+         // if (!result.ok) {
+         //   Alert.alert('Error', 'Este correo no vale');
+         // }
+         //  setUserTokens(result.token, '');
+         console.log(UserReducer.default(this.state.prueba, setUserTokens(result.token, result.token)));
+         console.log(UserReducer.default(this.state.prueba, ''));
+         this.setState({ spinner: false });
+       });
+     }
+   };
 
-  return (
-    <View>
-      {/* <Spinner
-        visible={spinner}
-        textContent={'Cargando...'}
-      /> */}
+   render() {
+     return (
+       <View>
+         { this.state.spinner
+        && (
+        <Spinner
+          visible
+          textContent="Cargando..."
+        />
+        )}
 
-      <TextInput
-        placeholder="Email"
-        onChangeText={onChangeEmail}
-        keyboardType="visible-password"
-      />
+         <TextInput
+           placeholder="Email"
+           onChangeText={(result) => this.setState({ email: result })}
+           keyboardType="visible-password"
+         />
 
-      <TextInput
-        placeholder="Contraseña"
-        keyboardType="visible-password"
-        secureTextEntry={true}
-        onChangeText={onChangePassword}
-      />
+         <TextInput
+           placeholder="Contraseña"
+           secureTextEntry
+           onChangeText={(result) => this.setState({ password: result })}
+         />
 
-      {/** Confirmar contraseña */}
+         {/** Confirmar contraseña */}
 
-      <Button onPress={onSubmit}>
-        <Text>Iniciar Sesión</Text>
-      </Button>
+         <Button onPress={this.onSubmit}>
+           <Text>Iniciar Sesión</Text>
+         </Button>
 
-    </View>
-  );
+       </View>
+     );
+   }
 }
+
+export default LoginContainer;
