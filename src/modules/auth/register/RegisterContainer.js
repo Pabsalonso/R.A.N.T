ee1570ee91d065868af52e51ddf8a31c1caff1c7
Pabsalonso <56518151+Pabsalonso@users.noreply.github.com>
2022-05-09 20:37:52 +0100

    Ampliada la pantalla del registro con los nuevos campos, y añadidos estilos

--- src/modules/auth/register/RegisterContainer.js
+++ src/modules/auth/register/RegisterContainer.js
@@ -1,58 +1,181 @@
-import { View, Text, Button } from 'native-base';
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable global-require */
+import { View, Text } from 'native-base';
 import React from 'react';
-import { TextInput } from 'react-native-gesture-handler';
+import BaseComponent from 'base/BaseComponent';
+import { connect } from 'react-redux';
+
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import * as FormValidator from '../../../utils/validators/FormValidators';
 // import * as Routing from 'routes/Routing';
-import { Alert, ActivityIndicator } from 'react-native';
+import { Alert, Image } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
+import ImagePickerModal from 'modules/imagePicker/ImagePickerModal';
+import * as ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { register } from 'services/api/ApiCalls';
+import { setUserTokens, setUserData } from '../../../services/user/UserActions';
 
-export default function RegisterContainer() {
-  const [spinner, setSpinner] = React.useState(false);
-  const [email, onChangeEmail] = React.useState('');
-  const [password, onChangePassword] = React.useState('');
+import { registerStyle } from './register.style';
 
-  const onSubmit = () => {
-    setSpinner(true);
-    if (!FormValidator.emailValidator(email)
-     || !FormValidator.isValidPassword(password)) {
-      Alert.alert('Error', 'Email o contraseña no cumple con requisitos');
-    } else {
-      register(email, password).then((result) => {
-        if (!result.ok) {
-          Alert.alert('Error', 'Este correo no vale');
-        }
-      });
-    }
-    setSpinner(false);
+class RegisterContainer extends BaseComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spinner: false,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      profile: '',
+      name: '',
+      visible: false,
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onImageLibraryPress = () => {
+    const options = {
+      selectionLimit: 1,
+      mediaType: 'photo',
+      includeBase64: true,
+    };
+    ImagePicker.launchImageLibrary(options).then((response) => {
+      this.setState({ profile: response.assets !== undefined ? response.assets[0].base64 : '' });
+      this.setState({ visible: false });
+    }).catch();
   };
 
-  return (
-    <View>
-      { spinner ? <ActivityIndicator color={"#fff"} /> : null }
-      {/* <Spinner
-        visible={spinner}
-        textContent={'Cargando...'}
-      /> */}
-      <Text>
-        Hola
-      </Text>
-      <TextInput
-        placeholder="Email"
-        onChangeText={onChangeEmail}
-      />
+  onCameraPress = () => {
+    const options = {
+      saveToPhotos: false,
+      mediaType: 'photo',
+      includeBase64: true,
+    };
+    ImagePicker.launchCamera(options).then((response) => {
+      this.setState({ profile: response.assets !== undefined ? response.assets[0].base64 : '' });
+      this.setState({ visible: false });
+    }).catch();
+  };
 
-      <TextInput
-        placeholder="Contraseña"
-        secureTextEntry={true}
-        onChangeText={onChangePassword}
-      />
+   onSubmit = ({ email, password, confirmPassword, profile, name }) => {
+     if (!FormValidator.emailValidator(email)
+       || !FormValidator.isValidPassword(password)) {
+       Alert.alert('Error', 'Email o contraseña no cumple con requisitos');
+     } else {
+       this.setState({ spinner: true });
+       register(email, password, confirmPassword, profile, name).then((result) => {
+         if (result.code === 401) {
+           Alert.alert('Error', 'El correo ya se encuentra en uso');
+           this.setState({ spinner: false });
+         } else {
+           this.props.setUserTokens(result.token, '');
+           this.props.setUserData(result);
+         }
+       });
+     }
+   };
 
-      <Button onPress={onSubmit}>
-        <Text>Prueba a registrarte</Text>
-      </Button>
+   render() {
+     const values = this.state;
+     return (
+       <ScrollView style={{ flex: 1 }}>
+         {/* Carga el spinner cuando llame a la api */}
+         { this.state.spinner
+        && (
+        <Spinner
+          visible
+          textContent="Cargando..."
+        />
+        )}
+         <View style={registerStyle.titleContainer}>
+           <Text style={registerStyle.title}> Registrarse</Text>
+         </View>
+         <Text style={registerStyle.subtitle}>Bienvenido! Proporcionanos los siguientes datos para darte de alta</Text>
 
-    </View>
-  );
+         <View style={registerStyle.imageContainer}>
+           <View style={registerStyle.profileImgContainer}>
+             <Image
+               source={values.profile.length !== 0
+                 ? { uri: `data:image/png;base64,${values.profile}` }
+                 : require('resources/assets/images/emptyImg.png')}
+               style={registerStyle.img}
+             />
+           </View>
+           <View>
+             <TouchableOpacity
+               activeOpacity={0.8}
+               style={registerStyle.addImgButton}
+               onPress={() => this.setState({ visible: true })}
+             >
+               <Icon name="add-a-photo" size={20} />
+
+               <Text>{' '}{values.profile === '' ? 'Añadir foto de Perfil' : 'Editar foto de Perfil'}{' '}</Text>
+             </TouchableOpacity>
+             { values.profile.length !== 0 && (
+               <TouchableOpacity
+                 activeOpacity={0.8}
+                 style={registerStyle.removeImgButton}
+                 onPress={() => this.setState({ profile: '' })}
+               >
+                 <Icon name="delete" color="white" size={20} />
+
+                 <Text style={{ color: 'white' }}>{' '}Eliminar foto{' '}</Text>
+               </TouchableOpacity>
+             )}
+           </View>
+         </View>
+
+         <View style={registerStyle.containerContent}>
+          <TextInput
+            placeholder="Nombre y apellidos"
+            onChangeText={(result) => this.setState({ name: result })}
+            keyboardType="visible-password"
+            style={registerStyle.input}
+          />
+          <TextInput
+            placeholder="Email"
+            onChangeText={(result) => this.setState({ email: result })}
+            keyboardType="visible-password"
+            style={registerStyle.input}
+          />
+          <TextInput
+            placeholder="Contraseña"
+            secureTextEntry
+            onChangeText={(result) => this.setState({ password: result })}
+            style={registerStyle.input}
+          />
+          <TextInput
+            placeholder="Repetir contraseña"
+            secureTextEntry
+            onChangeText={(result) => this.setState({ confirmPassword: result })}
+            style={registerStyle.input}
+          />
+         {/** Falta Validatorss */}
+
+            <TouchableOpacity
+               activeOpacity={0.8}
+               style={registerStyle.registerButton}
+               onPress={ () => this.onSubmit(this.state) }
+             >
+               <Text style={registerStyle.registerButtonFont}>{' '}Crear cuenta{' '}</Text>
+             </TouchableOpacity>
+         </View>
+
+         <ImagePickerModal
+           isVisible={values.visible}
+           onClose={() => this.setState({ visible: false })}
+           onImageLibraryPress={this.onImageLibraryPress}
+           onCameraPress={this.onCameraPress}
+         />
+       </ScrollView>
+     );
+   }
 }
+
+const mapStateToPropsActions = {
+  setUserTokens,
+  setUserData,
+};
+
+export default connect(null, mapStateToPropsActions)(RegisterContainer);
---
+++ src/modules/auth/register/register.style.js
@@ -0,0 +1,85 @@
+import { StyleSheet } from 'react-native';
+
+export const registerStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    marginVertical: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: 'SendFlowers-Regular',
+  },
+  subtitle: {
+    fontSize: 15,
+    marginHorizontal: 20,
+    textAlign: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: '10%',
+  },
+  profileImgContainer: {
+    overflow: 'hidden',
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    borderWidth: 2,
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+  },
+  addImgButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F0D500',
+    marginLeft: '10%',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 20,
+  },
+  removeImgButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#CC0202',
+    marginTop: '5%',
+    marginLeft: '10%',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 20,
+  },
+  input: {
+    borderColor: 'grey',
+    width: '90%',
+    borderBottomWidth: 2,
+    padding: 10,
+    marginVertical: 10,
+  },
+  registerButton:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F0D500',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 20,
+    marginVertical: '10%',
+    width: 200,
+  },
+  registerButtonFont: {
+    fontSize: 25,
+    fontFamily: 'SendFlowers-Regular',
+  },
+});
